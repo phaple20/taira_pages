25533b514ec69483e45ae759844dbac100a63ea7
phaple <276348586@qq.com>
2020-08-09 22:26:20 +0800

    单击和长按事件

--- taira_cube/src/bag/Index/Index.vue
+++ taira_cube/src/bag/Index/Index.vue
@@ -1,47 +1,74 @@
 <template>
   <section>
-    <van-dropdown-menu active-color="#ff0000">
-      <van-dropdown-item v-model="value1" :options="this.$store.state.sh.city_sh" />
-      <van-dropdown-item v-model="value2" :options="option2" />
-    </van-dropdown-menu>
-    </div>
-    <a href="baidu.com">df</a>
-    <van-button plain type="primary">朴素按钮</van-button>
-    <van-icon name="chat-o" badge="99+" />
-    <van-cell-group title="分组1">
-      <van-cell title="cell" icon="shop-o">
-        <!-- 使用 right-icon 插槽来自定义右侧图标 -->
-        <template #right-icon>
-          <van-icon name="search" class="search=icon" />
-        </template>
-      </van-cell>
-      <!--arrow-direction="down"-->
-      <van-cell title="popup" is-link value="内容" @click="showPopup" />
-    </van-cell-group>
-    <van-popup
-      v-model="show"
-      position="left"
-      :style="{ width: '50%', height: '100%' }"
-      get-container="body"
-      closeable
-      round
-      :duration="0.3"
-      >content</van-popup
-    >
-    {{ testvuex }}
-    <van-row>
-      <van-col span="8">{{ countDouble }}</van-col>
-      <van-col span="8"
-        ><van-button plain type="primary" @click="addcount">add</van-button>
-      </van-col>
-      <van-col span="8"
-        ><van-button plain type="primary" @click="loading">load</van-button>
-      </van-col>
-    </van-row>
+    <van-skeleton :row="3" :loading="loading">
+      <van-dropdown-menu active-color="#ff0000">
+        <van-dropdown-item
+          v-model="value1"
+          :options="this.$store.state.sh.city_sh"
+        />
+        <van-dropdown-item v-model="value2" :options="option2" />
+      </van-dropdown-menu>
+      <a href="baidu.com">df</a>
+      <van-button plain type="primary">朴素按钮</van-button>
+      <van-icon name="chat-o" badge="99+" />
+      <van-cell-group title="分组1">
+        <van-cell title="cell" icon="shop-o">
+          <!-- 使用 right-icon 插槽来自定义右侧图标 -->
+          <template #right-icon>
+            <van-icon name="search" class="search=icon" />
+          </template>
+        </van-cell>
+        <!--arrow-direction="down"-->
+        <van-cell title="popup" is-link value="内容" @click="showPopup" />
+      </van-cell-group>
+      <van-popup
+        v-model="show"
+        position="left"
+        :style="{ width: '50%', height: '100%' }"
+        get-container="body"
+        closeable
+        round
+        :duration="0.3"
+        >content</van-popup
+      >
+      {{ testvuex }}
+      <van-row>
+        <van-col span="8">{{ countDouble }}</van-col>
+        <van-col span="8"
+          ><van-button plain type="primary" @click="addcount">add</van-button>
+        </van-col>
+        <van-col span="8"
+          ><van-button plain type="primary" @click="loadbtn">load</van-button>
+        </van-col>
+      </van-row>
+      <van-grid :border="false" :column-num="3">
+        <van-grid-item>
+          <div @touchstart="gotouchstart" @touchend="gotouchend">
+            <van-image src="https://img.yzcdn.cn/vant/apple-1.jpg" />
+          </div>
+        </van-grid-item>
+        <van-grid-item>
+          <van-image src="https://img.yzcdn.cn/vant/apple-2.jpg" />
+        </van-grid-item>
+        <van-grid-item>
+          <van-image src="https://img.yzcdn.cn/vant/apple-3.jpg" />
+        </van-grid-item>
+      </van-grid>
+      <van-grid>
+        <van-grid-item
+          v-for="value in 6"
+          :key="value"
+          icon="photo-o"
+          text="文字"
+        >
+        </van-grid-item>
+      </van-grid>
+    </van-skeleton>
   </section>
 </template>
 <script>
 import {
+  Skeleton,
   DropdownMenu,
   DropdownItem,
   Col,
@@ -49,20 +76,25 @@ import {
   Cell,
   CellGroup,
   Button,
+  Image,
+  Grid,
+  GridItem,
   Icon,
   Popup,
   Toast
 } from "vant";
 import { mapGetters } from "vuex";
+var timeOutEvent = 0;
 export default {
   computed: {
-    ...mapGetters(["countDouble","sh_area"]),
+    ...mapGetters(["countDouble", "sh_area"]),
     testvuex() {
       return this.$store.state.name;
-    },
+    }
   },
   data() {
     return {
+      loading: true,
       show: false,
       value1: 0,
       value2: "a",
@@ -77,18 +109,23 @@ export default {
   components: {
     [DropdownMenu.name]: DropdownMenu,
     [DropdownItem.name]: DropdownItem,
+    [Skeleton.name]: Skeleton,
     [Col.name]: Col,
     [Row.name]: Row,
     [Cell.name]: Cell,
+    [Grid.name]: Grid,
+    [GridItem.name]: GridItem,
     [CellGroup.name]: CellGroup,
     [Button.name]: Button,
+    [Image.name]: Image,
     [Icon.name]: Icon,
     [Popup.name]: Popup,
     [Toast.name]: Toast
   },
   created() {
-    this.$store.dispatch('Sh_area_get').then(() => {
-      console.log('suc')
+    this.$store.dispatch("Sh_area_get").then(() => {
+      console.log("finish");
+      this.loading = false;
     });
   },
   methods: {
@@ -100,19 +137,38 @@ export default {
         n: 10
       });
     },
-    loading() {
+    loadbtn() {
       Toast.loading({
         message: "加载中..."
         //forbidClick: true
       });
+    },
+    gotouchstart(e) {
+      e.preventDefault();
+      clearInterval(timeOutEvent);
+      this.status = true;
+      timeOutEvent = 0;
+      timeOutEvent = setTimeout(() => {
+        this.status = false;
+        Toast("长按了");
+      }, 1500);
+    },
+    gotouchend(e) {
+      clearInterval(timeOutEvent);
+      //console.log(this.status);
+      if (this.status) {
+        Toast("点击了");
+      } else {
+        //Toast("长按了");
+        this.status = true;
+      }
     }
   },
   mounted() {
+    console.log("moun");
     //this.option1 = this.$store.state.sh.city_sh
   },
-  watch: {
-    
-  },
+  watch: {}
 };
 </script>
 <style lang="scss" scoped>
--- taira_cube/src/store/module/module_weather_shanghai.js
+++ taira_cube/src/store/module/module_weather_shanghai.js
@@ -11,33 +11,36 @@ const module_sh = {
   },
   actions: {
     Sh_area_get({ commit }) {
-      axios
-        .get(
-          "http://api.k780.com/?app=weather.city&cou=1&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json"
-        )
-        .then(function(res) {
-          let city_json = res.data.result.datas;
-          let city_arr = [],
-            sh_arr = [];
-          for (var i in city_json) {
-            city_arr.push(city_json[i]);
-          }
-          let city_filter = city_arr.filter(item => {
-            return item.area_1 === "上海";
+      return new Promise((resolve, reject) => {
+        axios
+          .get(
+            "http://api.k780.com/?app=weather.city&cou=1&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json"
+          )
+          .then(function(res) {
+            let city_json = res.data.result.datas;
+            let city_arr = [],
+              sh_arr = [];
+            for (var i in city_json) {
+              city_arr.push(city_json[i]);
+            }
+            let city_filter = city_arr.filter(item => {
+              return item.area_1 === "上海";
+            });
+            city_filter.forEach((item, index) => {
+              //item.text = item.citynm;
+              //item.value = index;
+              sh_arr[index] = {};
+              sh_arr[index]["text"] = item.citynm;
+              sh_arr[index]["value"] = index;
+            });
+            //console.log(Array.from(new Set([1, 3, 4, 4, 4, 4, 4, 32, 54, 5])));
+            commit("sh_area_get", sh_arr);
+            resolve();
+          })
+          .catch(function(err) {
+            console.log(err);
           });
-          city_filter.forEach((item, index) => {
-            //item.text = item.citynm;
-            //item.value = index;
-            sh_arr[index] = {};
-            sh_arr[index]["text"] = item.citynm;
-            sh_arr[index]["value"] = index;
-          });
-          //console.log(Array.from(new Set([1, 3, 4, 4, 4, 4, 4, 32, 54, 5])));
-          commit("sh_area_get", sh_arr);
-        })
-        .catch(function(err) {
-          console.log(err);
-        });
+      });
     }
   },
   getters: {
